b0890f0d686c1e1ec2614027efb6ee22267384f1
Gurwinder Singh <gurwinder958652@gmail.com>
2019-06-06 21:04:36 -0400

    Initial Commit

---
+++ Day6June/main.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  Day6June
+//
+//  Created by MacStudent on 2019-06-06.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dog😱cow"
+
+let 🥰="hello"
+
+print(🥰,🐶🐮 )
+print(Int.max,Int.min)
+
+let a:Int
+let b:Int
+a=10
+b=3
+
+var add=a+b;
+print("Addition is \(add)")
+
+var sub=a-b
+print("Subtraction is \(sub)")
+
+var mul=a*b
+print("Multiplication is \(mul)")
+
+var div=a-b
+print("Division is \(div)")
