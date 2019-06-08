4b2e86d091b08aa5d5d0245f39724ab624710e37
Gurwinder Singh <gurwinder958652@gmail.com>
2019-06-07 20:49:34 -0400

    [All code]...Many

--- Day6June/main.swift
+++ Day6June/main.swift
@@ -40,15 +40,91 @@ print("Gurwinder Singh")
 
 var str="Gurwinder"
 
-if str.isEmpty
+if !str.isEmpty
 {
     print (str)
+    let s=str
+    str=str.lowercased()
+    print(str,s)
     
 }
+
+let index = str.firstIndex(of: ",") ?? str.endIndex
+print(index)
 let len=str.count
 print("Length of the String is \(len)")
 
 let rev=String(str.reversed())
 
 print("The reversed string is \(rev)")
+
+let c:Character
+
+c="H"
+
+print(c)
+/*
+for i in str.indices
+{
+    //print((i), separator: "", terminator: "")
+    print([i], separator: " ", terminator: " ")
+}
+
+//str.append("???")
+
+
+print(str)
+
+*/
+/*let quotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
+*/
+let dollarSign = "\u{24}"        // $,  Unicode scalar U+0024
+let blackHeart = "\u{2665}"      // ♥,  Unicode scalar U+2665
+let sparklingHeart = "\u{1F496}" // 💖, Unicode scalar U+1F496
+
+print()
+print(dollarSign, blackHeart, sparklingHeart , separator:"")
+
+func welcome()  {
+    print("Welcome To Toronto");
+}
+welcome();
+
+func greetings(name: String){
+    print("Welcome, \(name)")
+}
+greetings(name: "Myself Gurwinder Singh")
+
+/*for greetings(_ name:String,n: Int){
+for _ in 1...n{
+    print("@singhg1696, \(name)");
+}
+}*/
+
+//Tupls
+
+let x=(100,200)
+
+print(x);
+print(x.0,x.1)
+//quadratic conditions x y axis
+
+let y=(a:100,b:200)
+
+print(y);
+print(y.a,y.b)
+
+let(_, x1)=x
+
+print(x1)
+
+// Find the minimum number
+
+
 
